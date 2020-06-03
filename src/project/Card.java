dd68d39e2b7b61db26e0a9c32b482445128900b1
Ratinder2315 <bhulrati@sheridancollege.ca>
2020-06-03 15:58:23 -0400

    Starter codes

---
+++ src/project/Card.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Card Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package project;
+
+/**
+ *
+ * @author Ratinder Bhullar
+ */
+public abstract class Card 
+{
+   //default modifier for child classes
+    
+    /**
+     * Students should implement this method for their specific children classes 
+     * @return a String representation of a card. Could be an UNO card, a regular playing card etc.
+     */
+    
+    @Override
+    public abstract String toString();
+    
+}
